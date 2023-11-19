e1912d5d15a7d6bcc979c2fd9f9d45e1a0d50510
Imjustyuga <43130407+DeimosIT@users.noreply.github.com>
2023-11-20 09:58:37 +1100

    Update Avito-parser.py

--- Avito-parser.py
+++ Avito-parser.py
@@ -16,15 +16,39 @@ def get_page():
     try:
         driver = webdriver.Chrome(options=options)
         driver.get('https://www.avito.ru/yuzhno-sahalinsk/kvartiry/prodam-ASgBAgICAUSSA8YQ?context=H4sIAAAAAAAA_0q0MrSqLraysFJKK8rPDUhMT1WyLrYyNLNSKk5NLErOcMsvyg3PTElPLVGyrgUEAAD__xf8iH4tAAAA&f=ASgBAQICAUSSA8YQAUDKCCSAWYJZ')
-        time.sleep(10)
-        with open ('Source_selenium.html', 'w', encoding='UTF-8') as file:
-            file.write(driver.page_source)
-            file.close()
+        time.sleep(100)
+        page_count = int(driver.find('div', class_ = 'js-pages pagination-pagination-_FSNE').find_all('a')[-2].text)
+        for i in range (1, page_count + 1):
+            url = f'https://www.avito.ru/yuzhno-sahalinsk/kvartiry/prodam-ASgBAgICAUSSA8YQ?f=ASgBAQICAUSSA8YQAUDKCCSAWYJZ&p={i}'
+
+            driver.get(url=url)
+            with open (f'page_{1}.html', 'w', encoding='UTF-8') as file:
+                file.write(driver.page_source)
+                file.close()
     except Exception as ex:
         print(ex)
     finally:
         driver.close()
         driver.quit()
+
+def pagination():
+    headers = {
+        'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.837 YaBrowser/23.9.4.837 Yowser/2.5 Safari/537.36'
+    }
+    with open('Source_selenium.html', encoding='UTF-8') as file:
+        src = file.read()
+    soup = BeautifulSoup(src, 'lxml')
+    page_count = int(soup.find('div', class_ = 'js-pages pagination-pagination-_FSNE').find_all('a')[-2].text)
+
+    for i in range(1, page_count + 1):
+        url = f'https://www.avito.ru/yuzhno-sahalinsk/kvartiry/prodam-ASgBAgICAUSSA8YQ?f=ASgBAQICAUSSA8YQAUDKCCSAWYJZ&p={i}'
+
+        r = requests.get(url=url, headers=headers)
+        with open (f'data/page_{i}.html', 'w', encoding='UTF-8') as file:
+            file.write(r.text)
+        
+        time.sleep(2)
+    return page_count
 
 # Вывод стоимости квартир на первой странице
 def get_data():
@@ -41,8 +65,9 @@ def get_data():
    
 
 def main():
-    #get_page()
-    get_data()
+    get_page()
+    #get_data()
+    #pagination()
 
 if __name__ == '__main__':
     main()
